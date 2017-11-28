ce8ea12418573af1a3b3db26096184cbfb0f6812
boxshadow <idafoo@sina.com>
2017-11-28 10:06:32 +0800

    及时赠送礼物接口调整返回内容

--- src/API/Controllers/LiveGiftController.php
+++ src/API/Controllers/LiveGiftController.php
@@ -5,6 +5,7 @@ namespace Slimkit\PlusLive\API\Controllers;
 use Zhiyi\Plus\Models\User;
 use Illuminate\Http\Request;
 use Zhiyi\Plus\Models\WalletCharge;
+use Zhiyi\Plus\Models\CommonConfig;
 use Slimkit\PlusLive\Models\LiveUserInfo;
 
 class LiveGiftController extends BaseController
@@ -17,10 +18,10 @@ class LiveGiftController extends BaseController
     }
 
     // 请求赠送礼物
-    public function handleGift (Request $request, LiveUserInfo $liveModel, User $userModel, WalletCharge $charge)
+    public function handleGift (Request $request, LiveUserInfo $liveModel, User $userModel, WalletCharge $charge, CommonConfig $config)
     {
         if (!$this->is_ZhiboService($request)) {
-            return response()->json(['message' => '授权错误'])->setStatusCode(401);
+            return response()->json(['status' => 0, 'message' => '授权错误'])->setStatusCode(200);
         }
 
         $data = $request->only(['num', 'to_usid', 'usid', 'type', 'order', 'description', 'ctime', 'order_type']);
@@ -32,12 +33,17 @@ class LiveGiftController extends BaseController
         $liveUser = $userModel->newQuery()->find($liveUser);
 
         if ($liveUser->wallet->balance < $data['num']) {
-            return response()->json(['message' => '余额不足'])->setStatusCode(403);
-        }
+            return response()->json(['status' => 0, 'message' => '余额不足'])->setStatusCode(200);
+        }  
+
+        // 获取转换比例
+        $ratio = $config->where('namespace', 'common')
+            ->where('name', 'wallet:ratio')
+            ->value('value') ?: 1000;
 
         $liveUser->getConnection()->transaction( function () use ($targetUser, $liveUser, $data, $charge) {
             // 扣除操作用户余额
-            $liveUser->wallet()->decrement('balance', $data['num']);
+            $liveUser->wallet()->decrement('balance', $data['num'] / $ratio * 10000);
             // 扣费记录
             $userCharge = clone $charge;
             $userCharge->channel = 'user';
@@ -51,7 +57,7 @@ class LiveGiftController extends BaseController
 
             if($targetUser->wallet) {
                 // 增加目标用户余额
-                $targetUser->wallet()->increment('balance', $data['num']);
+                $targetUser->wallet()->increment('balance', $data['num'] / $ratio * 10000);
 
                 $charge->user_id = $targetUser->id;
                 $charge->channel = 'user';
@@ -66,6 +72,6 @@ class LiveGiftController extends BaseController
             // 直播被送礼物只有交易记录，没有通知
         });
 
-        return response()->json('', 201);
+        return response()->json(['status' => 1, 'message' => '交易成功'], 200);
     }
 }
